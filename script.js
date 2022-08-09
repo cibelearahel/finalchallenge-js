adb8340d85a0cba6ada5871b7ef786a940a68d6e
Sarah Portela Salehi <98932726+SarahPortelaSalehi@users.noreply.github.com>
2022-08-09 10:36:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -13,9 +13,46 @@
                 this.matricula = form.matricula.value;
                 this.endereco = form.endereco.value;
                 this.curso = form.curso.value;
-                this.turno = form.hora.value;
+                this.hora = form.hora.value;
             }
         }
+
+        function saveTextAsFile()
+        {
+        var textToSave = document.getElementById('aluno.nome', 'aluno.idade', 'aluno.matricula','aluno.endereco', 'aluno.curso', 'aluno.hora').value;
+        var textToSaveAsBlob = new Blob([textToSave], {type:"text/plain"});
+        var textToSaveAsURL = window.URL.createObjectURL(textToSaveAsBlob);
+        var fileNameToSaveAs = document.getElementById("inputFileNameToSaveAs").value;
+
+        var downloadLink = document.createElement("a");
+        downloadLink.download = fileNameToSaveAs;
+        downloadLink.innerHTML = "Download File";
+        downloadLink.href = textToSaveAsURL;
+        downloadLink.onclick = destroyClickedElement;
+        downloadLink.style.display = "none";
+        document.body.appendChild(downloadLink);
+
+        downloadLink.click();
+        }
+
+        function destroyClickedElement(event)
+        {
+        document.body.removeChild(event.target);
+        }
+
+        function loadFileAsText()
+        {
+        var fileToLoad = document.getElementById("fileToLoad").files[0];
+
+        var fileReader = new FileReader();
+        fileReader.onload = function(fileLoadedEvent)
+        {
+        var textFromFileLoaded = fileLoadedEvent.target.result;
+        document.getElementById("inputTextToSave").value = textFromFileLoaded;
+        };
+        fileReader.readAsText(fileToLoad, "UTF-8");
+        }
+
 
         function validar(form) {
         
@@ -55,3 +92,7 @@
                 }
 
             }
+        
+        
+            
+        
